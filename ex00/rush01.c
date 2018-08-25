8dc66ecfda979e47e6b67d0578e13cf78a961f88
gquintin <geremyquintin@outlook.be>
2018-08-25 17:01:40 +0200

    rush01.c

--- ex00/rush01.c
+++ ex00/rush01.c
@@ -6,36 +6,46 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/11 09:20:34 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/12 03:59:35 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/25 17:01:24 by gquintin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_putchar(char c);
+char	*write_line(char *str, int l, int x, int y)
+{
+	int c;
 
-void	rush(int x, int y)
+	c = 0;
+	while (c < x)
+	{
+		if (c + l == 0 || c + l == x + y - 2)
+			str[c] = '/';
+		else if ((l == 0 && c == x - 1) || (l == y - 1 && c == 0))
+			str[c] = '\\';
+		else if (l == 0 || l == y - 1 || c == 0 || c == x - 1)
+			str[c] = '*';
+		else
+			str[c] = ' ';
+		c++;
+	}
+	return (str);
+}
+
+char	*rush(int x, int y, char *str)
 {
 	int	l;
-	int	c;
+	int i;
 
-	if (x <= 0)
-		return ;
+	if (x <= 0 || y <= 0)
+		return ((void*)0);
 	l = 0;
+	i = 0;
 	while (l < y)
 	{
-		c = 0;
-		while (c < x)
-		{
-			if (c + l == 0 || c + l == x + y - 2)
-				ft_putchar('/');
-			else if ((l == 0 && c == x - 1) || (l == y - 1 && c == 0))
-				ft_putchar('\\');
-			else if (l == 0 || l == y - 1 || c == 0 || c == x - 1)
-				ft_putchar('*');
-			else
-				ft_putchar(' ');
-			c++;
-		}
-		ft_putchar('\n');
+		str = write_line(str + i, l, x, y);
+		i += x;
+		str[i] = '\n';
+		i++;
 		l++;
 	}
+	return (str);
 }
